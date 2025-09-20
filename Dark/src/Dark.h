074782cce7d65c4ef598c2e1bd77b18ee9cf75df
John Lv <lvwenzhuang@126.com>
2025-09-20 21:13:24 +0800

    setup basic Application and EntryPoint

---
+++ Dark/src/Dark.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Dark Engine applications
+
+#include "Dark/Core.h"
+#include "Dark/Application.h"
+
+// ---- Entry Point ----
+#include "Dark/EntryPoint.h"
---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,19 @@
+#include <Dark.h>
+
+class Sandbox : public Dark::Application
+{
+public:
+	Sandbox()
+	{
+	}
+
+	~Sandbox()
+	{
+	}
+
+};
+
+Dark::Application* Dark::CreateApplication()
+{
+	return new Sandbox();
+}
